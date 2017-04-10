5cc8819f8c8d8dbd5778ae49939cfbaba4edbfad
Victor <victor.golubei@gmail.com>
2017-04-10 17:19:36 +0200

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Collateral dream team</h1><h2>App works!</h2>'
+})
+export class AppComponent {
+
+}
